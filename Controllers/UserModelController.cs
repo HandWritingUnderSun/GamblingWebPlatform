f511bf99f06c934c68691dfeb8837499e5a5f7d5
zhu <zhuy1@126.com>
2020-07-04 01:37:39 +0800

    v2

---
+++ Controllers/UserModelController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GamblingWebPlatform.Controllers
+{
+    public class UserModelController :ControllerBase
+    {
+
+        private readonly ILogger<UserModelController> _logger;
+
+        public UserModelController(ILogger<UserModelController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public UserModel Get()
+        {
+            return new UserModel("zhuyu","1234");
+        }
+    }
+}
---
+++ UserModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GamblingWebPlatform
+{
+    public class UserModel
+    {
+        public string UserName { get; set; }
+
+        public string UserPassword { get; set; }
+
+
+        public UserModel(string name,string password)
+        {
+            UserName = name;
+            UserPassword = password;
+        }
+    }
+}
