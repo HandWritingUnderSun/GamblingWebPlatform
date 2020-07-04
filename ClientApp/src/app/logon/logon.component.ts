bd1e7eaa02ec370988bef2229a27c5b5b9599eb0
zhu <zhuy1@126.com>
2020-07-05 01:00:38 +0800

    v3

--- ClientApp/src/app/logon/logon.component.ts
+++ ClientApp/src/app/logon/logon.component.ts
@@ -7,14 +7,15 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./logon.component.css']
 })
 export class LogonComponent {
-  public usermodel: UserModel;
+  usermodel: any;
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     http.get<UserModel>(baseUrl + 'api/UserModelController/UserModel').subscribe(result => {
-      this.usermodel = result;
+      this.usermodel = {username:'123',userpassword:'123'};
+      alert(this.usermodel);
     }, error => console.error(error));
   }
-}
+} 
 
 interface UserModel {
   username: string;
